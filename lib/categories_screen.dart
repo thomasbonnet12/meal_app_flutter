3d3b5934f187ef9ef717fa4ac62890fcd68995fd
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-02-04 21:39:39 +0100

    category item.dart created. dummydata and categoryitem object used in categories_screen.dart to be map upon

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app_flutter/category_item.dart';
+import 'package:meal_app_flutter/data_base.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key key}) : super(key: key);
@@ -6,7 +8,9 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
